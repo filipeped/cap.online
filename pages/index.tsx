d2711ca7b5334c584a0bd3cf9fcac4c54e449a60
filipeped <filipepedoo@gmail.com>
2025-07-15 21:40:42 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       event_name: "Lead",
       event_time: Math.floor(Date.now() / 1000),
       action_source: "website",
-      event_source_url: "https://www.digitalpaisagismo.com.br",
+      event_source_url: "https://www.digitalpaisagismo.online",
       user_data: {
         external_id: "dec28dba1ef8f7a974d0daa5fb417e886d608ff870dea037176fafd3ef931045",
         client_ip_address: "177.155.123.123",
@@ -35,7 +35,7 @@ export default function Home() {
 
     const payload = {
       data: [event],
-      pixel_id: "1142320931265624",
+      pixel_id: "747466124434832",
       access_token: "EAAQfmxkTTZCcBPJqdYzaTyZB5WWFIMaXGDF9WhWWbgbO4jhifEM5l25TvjYzaBPT3QoZBiYG5cIxJnpHIQrxZCX7HUOlXcXX5yrCbdJIOD8fBcZAIpM9QSwiGo4gYTZA3AAtdrM5V38LLt4td6oW6ou6eCGzecRZBfSIev4yH258aQEZBdR3gBrgFrQZBAOoJTQZDZD"
     };
 
